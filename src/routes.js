06cbee9e062826db7b8bad38dbdce06fee4ceb13
Junio MM <info@juniomarques.com>
2020-02-22 21:05:02 +0100

    Add: rotas (agendar visitar, listar visitas) controler de agendamento por tempo

--- src/routes.js
+++ src/routes.js
@@ -1,13 +1,16 @@
 import { Router } from 'express';
 import multer from 'multer';
+import { RuleTester } from 'eslint';
 import multerConfig from './config/multer';
 
 import UserController from './app/controllers/UserController';
 import SessionControlle from './app/controllers/SessionController';
 import FileController from './app/controllers/FileController';
 import ProviderController from './app/controllers/ProviderController';
+import ScheduleController from './app/controllers/ScheduleController';
 
 import authMiddleware from './app/middlewares/auth';
+import AppointmentController from './app/controllers/AppointmentController';
 
 const routes = new Router();
 const upload = multer(multerConfig);
@@ -18,7 +21,13 @@ routes.post('/sessions', SessionControlle.store);
 routes.use(authMiddleware);
 routes.put('/users', UserController.update);
 
-routes.post('/files', upload.single('file'), FileController.store);
 routes.get('/providers', ProviderController.index);
+
+routes.get('/appointments', AppointmentController.index);
+routes.post('/appointments', AppointmentController.store);
+
+routes.get('/schedule', ScheduleController.index);
+
+routes.post('/files', upload.single('file'), FileController.store);
 
 export default routes;
